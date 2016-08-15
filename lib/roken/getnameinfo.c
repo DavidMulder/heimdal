560ae2f4f83abf75efcbe8f1bac41440930ad7f7
John Bowers <John.Bowers@quest.com>
2016-08-15 14:02:47 -0600

    ifdef AIX solves some problems with 64bit AIX builds where apparently socklen_t is not typdeffed the same as size_t

--- lib/roken/getnameinfo.c
+++ lib/roken/getnameinfo.c
@@ -91,12 +91,20 @@ doit (int af,
  *
  */
 
+#ifdef _AIX
+int ROKEN_LIB_FUNCTION
+getnameinfo(const struct sockaddr *sa, size_t salen,
+           char *host, size_t hostlen,
+           char *serv, size_t servlen,
+           int flags)
+#else
 ROKEN_LIB_FUNCTION int ROKEN_LIB_CALL
 getnameinfo(const struct sockaddr *sa, socklen_t salen,
 	    char *host, size_t hostlen,
 	    char *serv, size_t servlen,
 	    int flags)
 {
+#endif
     switch (sa->sa_family) {
 #ifdef HAVE_IPV6
     case AF_INET6 : {
